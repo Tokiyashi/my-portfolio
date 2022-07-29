fdf14854b122525236b69ad36eca325a2eaec711
Tokiyashi <rogich6319@gmail.com>
2022-07-29 20:01:35 +0300

    added img

--- client/src/components/Sections/Cases.jsx
+++ client/src/components/Sections/Cases.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import me from './img/IM2G_2484.jpg'
+import cryptoapp from './img/cryptoapp.jpg'
 
 const Cases = ({reference}) => {
     return (
@@ -8,21 +8,22 @@ const Cases = ({reference}) => {
                 <h3> Case Studies: </h3>
                 <div className="case">
                     <div className="case__workInfo">
-                        <h4> Cryptocurrency App </h4>
+                        <h4> <a href="https://tokiyashi.github.io/Cryptocurrency/">Cryptocurrency App</a>  </h4>
                         <p> In this app you can see all popular crypto-coins. You can sort them and search anything you want. This project uses CoinGecko API.  </p>
                     </div>
                     <img
-                        src={me}
+                        src={cryptoapp}
                         className="case__img"
                     />
                 </div>
 
                 <div id="weather-app" className="case">
                     <div className="case__workInfo">
-                        <h4> Weather App </h4>
+                        <h4>  Weather App </h4>
                         <p> Very simple site that shows some information about weather in selected city. Also you will find a forecast for the next days. </p>
                     </div>
                     <img
+                        src={cryptoapp}
                         className="case__img"
                     />
                 </div>
